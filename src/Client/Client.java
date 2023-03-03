b6f7dc54b03e04a111eb01b61bc13adc933c024b
Jmtri7 <Jmtri7@gmail.com>
2023-03-03 09:11:55 -0500

    refactor

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -2,69 +2,48 @@ package Client;
 
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
 
-public class Client implements Runnable {
-	static final Scanner scanner = new Scanner(System.in);
+public class Client {
 	private final int port;
 	Socket socket;
 	private PrintWriter printWriter;
 	private BufferedReader bufferedReader;
-	private final Thread thread = new Thread(this);
-	private boolean quit = false;
 	public Client(int port) {
 		this.port = port;
 	}
-	public void start() {
+	public void connect() {
 		try {
-			this.socket = new Socket("localhost", 4444);
+			this.socket = new Socket("localhost", this.port);
 			this.printWriter = new PrintWriter(this.socket.getOutputStream(), true);
 			this.bufferedReader = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
-			System.out.println("===============");
-			System.out.println("CLIENT STARTED");
-			System.out.println("===============\n");
-			this.thread.start();
 		} catch(Exception e) {
 			System.out.println("No server started on port: " + this.port + "\n");
 		}
 	}
-	public void run() {
-		while (!this.quit) {
-			System.out.print("Send a message: ");
-			String message = Client.scanner.nextLine();
-			if(message.equals("quit")) {
-				this.quit = true;
-				return;
-			}
-			this.sendMessageToServer(message);
+	public boolean sendMessageToServer(String message) {
+		try {
+			this.printWriter.println(message);
+			return true;
+		} catch(Exception e) {
+			return false;
 		}
-
+	}
+	private String getServerMessage() {
+		try {
+			if(this.bufferedReader.ready()) return this.bufferedReader.readLine();
+			else return "";
+		} catch(Exception e) {
+			return null;
+		}
+	}
+	public void disconnect() {
 		try {
 			this.bufferedReader.close();
 			this.printWriter.close();
 			this.socket.close();
-			System.out.println("Socket closed!");
+			System.out.println("Client stopped!");
 		} catch (Exception e) {
-			System.out.println("Socket failed to close at port: " + this.port);
+			System.out.println("Failed to stop client!");
 		}
-	}
-	public void sendMessageToServer(String message) {
-		try {
-			this.printWriter.println(message);
-		} catch(Exception e) {
-			System.out.println("Failed to send message!");
-		}
-		System.out.println();
-	}
-	private String getServerMessage() {
-		try {
-			return this.bufferedReader.readLine();
-		} catch(Exception e) {
-			System.out.println("Failed to read server input");
-			return "";
-		}
-	}
-	public String getIP() {
-		return String.valueOf(this.socket.getInetAddress());
 	}
 }
--- src/ClientTest.java
+++
@@ -1,8 +0,0 @@
-import Client.Client;
-
-public class ClientTest {
-    public static void main(String[] args) {
-        Client client = new Client(4444);
-        client.start();
-    }
-}
